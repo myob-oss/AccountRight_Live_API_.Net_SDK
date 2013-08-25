e17ec10b906ce0709b964ade0aec02380088995d
Shaun Wilde <shaun_wilde@.rEm0vethIsB1t.hotmail.com>
2013-08-25 12:35:10 +1000

    upgrade opencover

--- build.bat
+++ build.bat
@@ -4,6 +4,6 @@ mkdir .\Artefacts\Coverage
 .\.nuget\Nuget.exe install .\.nuget\packages.config -outputdirectory packages
 %systemroot%\microsoft.net\framework\v4.0.30319\MSBuild.exe MYOB.API.SDK.sln
 
-.\packages\OpenCover.4.5.1604\OpenCover.Console.exe -register:user -filter:"+[MYOB.*]*" -output:".\Artefacts\Coverage\output.xml" -target:".\packages\NUnit.Runners.2.6.2\tools\nunit-console.exe" -targetargs:".\Artefacts\SDK\Test\Debug\SDK.Tests.dll /noshadow "
+.\packages\OpenCover.4.5.1923\OpenCover.Console.exe -register:user -filter:"+[MYOB.*]*" -skipautoprops -output:".\Artefacts\Coverage\output.xml" -target:".\packages\NUnit.Runners.2.6.2\tools\nunit-console.exe" -targetargs:".\Artefacts\SDK\Test\Debug\SDK.Tests.dll /noshadow "
 .\packages\ReportGenerator.1.9.0.0\ReportGenerator.exe -reports:.\Artefacts\Coverage\output.xml -targetdir:.\Artefacts\Coverage
 
