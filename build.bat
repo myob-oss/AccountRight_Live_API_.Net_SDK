096eb2eff617f0857ddefc0d2fccede0011842ef
Shaun Wilde <shaun_wilde@hotmail.com>
2013-12-20 10:02:42 +1100

    build as release package

--- build.bat
+++ build.bat
@@ -11,10 +11,10 @@ powershell -file sdk.project.check.ps1
 @IF NOT EXIST .\MYOB.API.SDK\SDK\MYOB.API.SDK.snk "%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.0A\Bin\sn.exe" -k .\MYOB.API.SDK\SDK\MYOB.API.SDK.snk
 @IF ERRORLEVEL 1 exit /b
 
-%systemroot%\microsoft.net\framework\v4.0.30319\MSBuild.exe MYOB.API.SDK.sln
+%systemroot%\microsoft.net\framework\v4.0.30319\MSBuild.exe MYOB.API.SDK.sln /p:Configuration=Release
 @IF ERRORLEVEL 1 exit /b
 
-.\packages\OpenCover.4.5.1923\OpenCover.Console.exe -register:user -filter:"+[MYOB.*]*" -skipautoprops -output:".\Artefacts\Coverage\output.xml" -target:".\packages\NUnit.Runners.2.6.2\tools\nunit-console.exe" -targetargs:".\Artefacts\SDK\Test\Debug\SDK.Tests.dll /noshadow "
+.\packages\OpenCover.4.5.1923\OpenCover.Console.exe -register:user -filter:"+[MYOB.*]*" -skipautoprops -output:".\Artefacts\Coverage\output.xml" -target:".\packages\NUnit.Runners.2.6.2\tools\nunit-console.exe" -targetargs:".\Artefacts\SDK\Test\Release\SDK.Tests.dll /noshadow "
 .\packages\ReportGenerator.1.9.0.0\ReportGenerator.exe -reports:.\Artefacts\Coverage\output.xml -targetdir:.\Artefacts\Coverage
 
 .\.nuget\Nuget.exe pack MYOB.API.SDK\SDK.Package\Package.nuspec
