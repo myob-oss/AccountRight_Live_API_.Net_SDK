9978506b9744bc1b9fde0a13fd9089113817030b
Shaun Wilde <shaun.wilde@myob.com>
2015-11-04 17:43:15 +1100

    enable COM Visibility

---
+++ MYOB.API.SDK/SDK.Test/COMSupportedTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using MYOB.AccountRight.SDK.Contracts.Version2.GeneralLedger;
+using NUnit.Framework;
+
+namespace SDK.Test
+{
+    [TestFixture]
+    public class COMSupportedTests
+    {
+        [Test]
+        public void AssemblySupportsCOM()
+        {
+            var attribute = (ComVisibleAttribute)typeof(Account).Assembly.GetCustomAttributes(typeof(ComVisibleAttribute), true).FirstOrDefault();
+
+            Assert.NotNull(attribute);
+            Assert.IsTrue(attribute.Value);
+        }
+    }
+}
