ffcd2fee5114e86415bf11e5d9963530bc8ae21e
Shaun Wilde <shaun_wilde@hotmail.com>
2016-02-09 07:27:49 +1100

    use same msbuild as used during compile

--- sonar.bat
+++ sonar.bat
@@ -7,7 +7,7 @@ IF "%appveyor_build_version%" == "" set appveyor_build_version=1.0.0
 .\tools\sonarqube\runner\MSBuild.SonarQube.Runner.exe begin /d:sonar.login=%SONARQUBE_USER% /d:sonar.password=%SONARQUBE_PASSWORD% /d:sonar.host.url=http://sonar.many-monkeys.com:9000  /d:sonar.cs.opencover.reportsPaths=".\Artefacts\Coverage\output.xml" /n:"myob-sdk" /k:"myob-sdk" /v:"%appveyor_build_version%"
 @IF ERRORLEVEL 1 exit /b
 
-msbuild MYOB.API.SDK.sln /p:Configuration=release /t:rebuild
+%systemroot%\microsoft.net\framework\v4.0.30319\MSBuild.exe MYOB.API.SDK.sln /p:Configuration=release /t:rebuild
 @IF ERRORLEVEL 1 exit /b
 
 .\tools\sonarqube\runner\MSBuild.SonarQube.Runner.exe end
