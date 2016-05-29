d3f99eefb948a8ad35ef93d22e913e61f3edb296
Shaun Wilde <shaun.wilde@myob.com>
2016-05-30 09:48:33 +1000

    change sonar keyname

--- sonar.bat
+++ sonar.bat
@@ -6,7 +6,7 @@ IF "%SONARQUBE_NEMO_TOKEN%" == "" goto TheEnd
 
 IF "%appveyor_build_version%" == "" set appveyor_build_version=1.0.0
 
-.\tools\sonarqube\runner\MSBuild.SonarQube.Runner.exe begin /d:sonar.login=%SONARQUBE_NEMO_TOKEN% /d:sonar.host.url=https://nemo.sonarqube.org /d:sonar.cs.opencover.reportsPaths=".\Artefacts\Coverage\output.xml" /n:"myob-sdk" /k:"myob-sdk" /v:"%appveyor_build_version%"
+.\tools\sonarqube\runner\MSBuild.SonarQube.Runner.exe begin /d:sonar.login=%SONARQUBE_NEMO_TOKEN% /d:sonar.host.url=https://nemo.sonarqube.org /d:sonar.cs.opencover.reportsPaths=".\Artefacts\Coverage\output.xml" /n:"myob-ar-dotnet-sdk" /k:"myob-ar-dotnet-sdk" /v:"%appveyor_build_version%"
 @IF ERRORLEVEL 1 exit /b
 
 "%programfiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe" MYOB.API.SDK.sln /p:Configuration=release /t:rebuild
