a13763093a35c40457cd92143b9f5644939943b9
Shaun Wilde <shaun.wilde@myob.com>
2016-05-30 09:45:23 +1000

    update to use sonarqube nemo

--- sonar.bat
+++ sonar.bat
@@ -2,12 +2,11 @@
 
 .\packages\coveralls.io.1.3.4\tools\coveralls.net.exe --opencover .\Artefacts\Coverage\output.xml
 
-IF "%SONARQUBE_USER%" == "" goto TheEnd
-IF "%SONARQUBE_PASSWORD%" == "" goto TheEnd
+IF "%SONARQUBE_NEMO_TOKEN%" == "" goto TheEnd
 
 IF "%appveyor_build_version%" == "" set appveyor_build_version=1.0.0
 
-.\tools\sonarqube\runner\MSBuild.SonarQube.Runner.exe begin /d:sonar.login=%SONARQUBE_USER% /d:sonar.password=%SONARQUBE_PASSWORD% /d:sonar.host.url=http://sonar.many-monkeys.com:9000  /d:sonar.cs.opencover.reportsPaths=".\Artefacts\Coverage\output.xml" /n:"myob-sdk" /k:"myob-sdk" /v:"%appveyor_build_version%"
+.\tools\sonarqube\runner\MSBuild.SonarQube.Runner.exe begin /d:sonar.login=%SONARQUBE_NEMO_TOKEN% /d:sonar.host.url=https://nemo.sonarqube.org /d:sonar.cs.opencover.reportsPaths=".\Artefacts\Coverage\output.xml" /n:"myob-sdk" /k:"myob-sdk" /v:"%appveyor_build_version%"
 @IF ERRORLEVEL 1 exit /b
 
 "%programfiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe" MYOB.API.SDK.sln /p:Configuration=release /t:rebuild
