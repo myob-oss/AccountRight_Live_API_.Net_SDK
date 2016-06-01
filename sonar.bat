9fc5f5beb3b7a2ad9626c6e9bef5c48982f7bad1
Shaun Wilde <shaun_wilde@hotmail.com>
2016-06-02 00:28:28 +1000

    update sonar token and use token for end step update runner

--- sonar.bat
+++ sonar.bat
@@ -4,15 +4,18 @@
 
 IF "%SONARQUBE_NEMO_TOKEN%" == "" goto TheEnd
 
+rmdir /s /q .sonarqube
+
 IF "%appveyor_build_version%" == "" set appveyor_build_version=1.0.0
 
-.\tools\sonarqube\runner\MSBuild.SonarQube.Runner.exe begin /d:sonar.login=%SONARQUBE_NEMO_TOKEN% /d:sonar.host.url=https://nemo.sonarqube.org /d:sonar.cs.opencover.reportsPaths=".\Artefacts\Coverage\output.xml" /n:"myob-sdk" /k:"myob-sdk" /v:"%appveyor_build_version%"
+rem /d:sonar.verbose=true
+.\tools\sonarqube\runner\MSBuild.SonarQube.Runner.exe begin /d:sonar.verbose=true /d:sonar.login=%SONARQUBE_NEMO_TOKEN% /d:sonar.host.url=https://nemo.sonarqube.org/ /d:sonar.cs.opencover.reportsPaths=".\Artefacts\Coverage\output.xml" /n:"myob-sdk" /k:"myob-sdk" /v:"%appveyor_build_version%"
 @IF ERRORLEVEL 1 exit /b
 
 "%programfiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe" MYOB.API.SDK.sln /p:Configuration=release /t:rebuild
 @IF ERRORLEVEL 1 exit /b
 
-.\tools\sonarqube\runner\MSBuild.SonarQube.Runner.exe end
+.\tools\sonarqube\runner\MSBuild.SonarQube.Runner.exe end /d:sonar.login=%SONARQUBE_NEMO_TOKEN%
 @IF ERRORLEVEL 1 exit /b
 
 :TheEnd
