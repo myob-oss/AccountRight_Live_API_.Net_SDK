3a9db18c5ffd7a87773cbd67b9aed071bf5a8679
Jordan Tan <jordan.tan@myob.com>
2018-09-06 08:04:27 +1000

    Add the new EmployeePaymentSummaryReport

---
+++ MYOB.API.SDK/SDK/Contracts/Version2/Contact/EmployeePaymentSummaryReport.cs
@@ -0,0 +1,20 @@
+﻿using MYOB.AccountRight.SDK.Contracts.Version2.Payroll;
+
+namespace MYOB.AccountRight.SDK.Contracts.Version2.Contact
+{
+    /// <summary>
+    /// Describes the EmployeePaymentSummaryReport
+    /// </summary>
+    public class EmployeePaymentSummaryReport : BaseEntity
+    {
+        /// <summary>
+        /// Employee
+        /// </summary>
+        public EmployeeLink Employee { get; set; }
+
+        /// <summary>
+        /// Employee payment summmary
+        /// </summary>
+        public EmployeePaymentSummary EmployeePaymentSummary { get; set; }
+    }
+}
---
+++ MYOB.API.SDK/SDK/Services/Version2/Contact/EmployeePaymentSummaryReportService.cs
@@ -0,0 +1,29 @@
+﻿using MYOB.AccountRight.SDK.Communication;
+using MYOB.AccountRight.SDK.Contracts.Version2.Contact;
+
+namespace MYOB.AccountRight.SDK.Services.Contact
+{
+    /// <summary>
+    /// A service that provides access to the <see cref="EmployeePaymentSummaryReport"/> resource
+    /// </summary>
+    public sealed class EmployeePaymentSummaryReportService : ReadableService<EmployeePaymentSummaryReport>
+    {
+        /// <summary>
+        /// Initialise a service that can use <see cref="EmployeePaymentSummaryReport"/> resources
+        /// </summary>
+        /// <param name="configuration">The configuration required to communicate with the API service</param>
+        /// <param name="webRequestFactory">A custom implementation of the <see cref="WebRequestFactory"/>, if one is not supplied a default <see cref="WebRequestFactory"/> will be used.</param>
+        /// <param name="keyService">An implementation of a service that will store/persist the OAuth tokens required to communicate with the cloud based API at http://api.myob.com/accountright </param>
+        public EmployeePaymentSummaryReportService(IApiConfiguration configuration, IWebRequestFactory webRequestFactory = null, IOAuthKeyService keyService = null)
+            : base(configuration, webRequestFactory, keyService)
+        {
+        }
+
+        /// <summary>
+        /// The route to the service (after the company file identifier)
+        /// </summary>
+        public override string Route
+        {
+            get { return "Contact/EmployeePaymentSummaryReport"; }
+        }}
+}
