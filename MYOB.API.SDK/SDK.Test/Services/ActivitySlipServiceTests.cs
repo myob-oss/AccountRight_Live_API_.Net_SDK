a01198d6ffda253919733fb64735815521bc81d5
Jasper Mondares <jaspermondie@users.noreply.github.com>
2018-11-21 09:25:22 +1100

    Updates around licence, Employee Payment Summary and Activity Slip endpoints (#73)
    * Add the new EmployeePaymentSummaryReport
    
    * Fix the csproj checks
    
    * Remove the the EmployeePaymentSummary property
    
    Remove the the EmployeePaymentSummary property from the EmployeePayrollDetails endpoint
    
    * Update for ActivitySlip (#72)
    
    * Update for ActivitySlip
    
    * Revert unintentional changes
    
    * Revery appveyor.yml
    
    * Update LICENSE

---
+++ MYOB.API.SDK/SDK.Test/Services/ActivitySlipServiceTests.cs
@@ -0,0 +1,15 @@
+﻿using MYOB.AccountRight.SDK.Services.TimeBilling;
+using NUnit.Framework;
+
+namespace SDK.Test.Services
+{
+    [TestFixture]
+    public class ActivitySlipServiceTests
+    {
+        [Test]
+        public void ServiceHasTheExpectedRoute()
+        {
+            Assert.AreEqual("TimeBilling/ActivitySlip", new ActivitySlipService(null, null).Route);
+        }
+    }
+}
