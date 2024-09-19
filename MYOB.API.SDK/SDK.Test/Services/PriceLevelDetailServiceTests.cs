a3714a12979c2dcc213f3935b4b11965be2e7e41
Bella Leber-Smeaton <bella.lebersmeaton@myob.com>
2024-09-19 14:19:17 +1000

    Generate SDK 24.8

---
+++ MYOB.API.SDK/SDK.Test/Services/PriceLevelDetailServiceTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using MYOB.AccountRight.SDK.Services.Inventory;
+using NUnit.Framework;
+
+namespace SDK.Test.Services
+{
+    [TestFixture]
+    public class PriceLevelDetailServiceTests
+    {
+        [Test]
+        public void ServiceHasTheExpectedRoute()
+        {
+            Assert.AreEqual("Inventory/PriceLevelDetail", new PriceLevelDetailService(null).Route);
+        }
+    } 
+}
---
+++ MYOB.API.SDK/SDK/Contracts/Version2/Inventory/PriceLevelDetail.cs
@@ -0,0 +1,23 @@
+﻿namespace MYOB.AccountRight.SDK.Contracts.Version2.Inventory
+{
+    /// <summary>
+    /// Price level detail
+    /// </summary>
+    public class PriceLevelDetail
+    {
+        /// <summary>
+        /// Name of the Price Level
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Value of the Price Level
+        /// </summary>
+        public string Value { get; set; }
+        
+        /// <summary>
+        /// row version number
+        /// </summary>
+        public string RowVersion { get; set; }
+    }
+}
